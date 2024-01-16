cb7b3ba61e87ef02bd2ce257640deeb8ac6ee565
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-16 18:26:12 +0530

    [experimental] added debounce function

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,24 +1,51 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
 	const [input, setInput] = useState(0);
+	const [expensiveValue, setExpensiveValue] = useState(0);
 	// Your solution starts here
 
+	useEffect(() => {
+		console.log(input);
+	}, [input]);
+
 	function factorial(num) {
+		console.log("reached factorial()");
+
 		let factorial = 1;
 
-		for (let i = num; i > 2; i--) {
-			console.log(`${i} * ${i - 1}`);
+		for (let i = num; i > 1; i--) {
+			// console.log(`${i} * ${i - 1}`);
 			factorial = factorial * i;
 		}
 
 		return factorial;
 	}
 
-	let expensiveValue = useMemo(() => factorial(input), [input]);
+	function debounce(fn) {
+		let timeout;
+
+		function random(...args) {
+			clearTimeout(timeout);
+
+			timeout = setTimeout(() => {
+				console.log("executing debounced func");
+				const result = fn(...args);
+
+				setExpensiveValue(result);
+			}, 1000);
+		}
+
+		return useMemo(() => random);
+	}
+
+	const debouncedFactorial = debounce(factorial);
+
+	useMemo(() => debouncedFactorial(input), [input]);
+
 	// Your solution ends here
 
 	return (
